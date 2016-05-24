47e2f91a90c35eb964cf7a164680c29ff09a1a35
krober16 <krober16@mail.uoguelph.ca>
2016-05-24 13:23:59 -0400

    Documentation
    File tools run description

--- FIleOperations/src/plugins/CopyFile.java
+++ FIleOperations/src/plugins/CopyFile.java
@@ -179,6 +179,9 @@ public class CopyFile implements WhiteboxPlugin {
         return amIActive;
     }
 
+    /**
+     * Used to execute this plugin tool.
+     */
     @Override
     public void run() {
         amIActive = true;
