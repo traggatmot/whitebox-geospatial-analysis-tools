8568c705252324389d23227a61d0841130060e18
krober16 <krober16@mail.uoguelph.ca>
2016-05-25 09:51:59 -0400

    Math Tools
    Run description fix

--- MathTools/src/plugins/Abs.java
+++ MathTools/src/plugins/Abs.java
@@ -184,6 +184,9 @@ public class Abs implements WhiteboxPlugin {
         return amIActive;
     }
 
+    /**
+     * Used to execute this plugin tool.
+     */
     @Override
     public void run() {
         amIActive = true;
