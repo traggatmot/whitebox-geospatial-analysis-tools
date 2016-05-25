46ca0bbf6504e680e5f524d32c4683008a60d525
krober16 <krober16@mail.uoguelph.ca>
2016-05-25 12:13:18 -0400

    File Operations
    Tool Descriptions

--- FIleOperations/src/plugins/DeleteFiles.java
+++ FIleOperations/src/plugins/DeleteFiles.java
@@ -21,7 +21,7 @@ import whitebox.interfaces.WhiteboxPlugin;
 import whitebox.interfaces.WhiteboxPluginHost;
 
 /**
- * WhiteboxPlugin is used to define a plugin tool for Whitebox GIS.
+ * This tool will delete user-specified input raster or vector data sets, including all associated files.
  *
  * @author Dr. John Lindsay email: jlindsay@uoguelph.ca
  */
